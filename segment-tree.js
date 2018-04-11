6505f76e0eceb7dc9f97c24f21f0da9f42c07709
Joe Crick <joe@josephcrick.com>
2018-04-11 18:28:23 +0200

    Fix syntax errors. Make private methods. Add tests.
    Add maximum value query.
    
    Add tests for object-based tree.

--- segment-tree.js
+++ segment-tree.js
@@ -1,38 +1,81 @@
+
+function build(index, from, to) {
+  this.heap[index] = new SegmentTreeNode();
+  this.heap[index].from = from;
+  this.heap[index].to = from + to - 1;
+  if (isLeafNode(to)) {
+    const leafVal = this.containsObjects ? this.array[from][this.propertyKey] : this.array[from];
+    this.heap[index].sum = leafVal;
+    this.heap[index].min = leafVal;
+    this.heap[index].max = leafVal;
+  } else {
+    build.call(this, 2 * index, from, (to / 2) | 0);
+    build.call(this, 2 * index + 1, from + ((to / 2) | 0), to - ((to / 2) | 0));
+    this.heap[index].sum = this.heap[2 * index].sum + this.heap[2 * index + 1].sum;
+    this.heap[index].min = Math.min(this.heap[2 * index].min, this.heap[2 * index + 1].min);
+    this.heap[index].max = Math.max(this.heap[2 * index].max, this.heap[2 * index + 1].max);
+  }
+}
+
+function isLeafNode(to) {
+  return to === 1;
+}
+
+function propagate(index) {
+  const node = this.heap[index];
+  if (node.pendingVal != null) {
+    change.call(this, this.heap[2 * index], node.pendingVal);
+    change.call(this, this.heap[2 * index + 1], node.pendingVal);
+    node.pendingVal = null;
+  }
+}
+
+function change(node, value) {
+  node.pendingVal = value;
+  node.sum = node.size() * value;
+  node.min = value;
+  node.max = value;
+  if (this.containsObjects) {
+    this.array[node.from][this.propertyKey] = value;
+  } else {
+    this.array[node.from] = value;
+  }
+}
+
+function contains(from1, to1, from2, to2) {
+  return from2 >= from1 && to2 <= to1;
+}
+
+function intersects(from1, to1, from2, to2) {
+  return (from1 <= from2 && to1 >= from2) || (from1 >= from2 && from1 <= to2);
+}
+
+function hasNodeBeenUpdated(node, from, to) {
+  return node.pendingVal != null && contains(node.from, node.to, from, to);
+}
+
 /**
  * Time-Complexity:  O(n*log(n))
- *
- * @param {Array} array the Initialization array
- * @class
  */
 module.exports = class SegmentTree {
-  constructor(array) {
+  constructor(array, containsObjects, propertyKey) {
     this.array = array;
     this.heap = [];
-    this.build(1, 0, array.length);
+    this.containsObjects = containsObjects;
+    this.propertyKey = propertyKey;
+    build.call(this, 1, 0, array.length);
   }
+
   size() {
     return this.array.length;
   }
-  build(index, from, size) {
-    this.heap[index] = new SegmentTreeNode();
-    this.heap[index].from = from;
-    this.heap[index].to = from + size - 1;
-    if (size === 1) {
-      this.heap[index].sum = this.array[from];
-      this.heap[index].min = this.array[from];
-    } else {
-      this.build(2 * index, from, (size / 2) | 0);
-      this.build(2 * index + 1, from + ((size / 2) | 0), size - ((size / 2) | 0));
-      this.heap[index].sum = this.heap[2 * index].sum + this.heap[2 * index + 1].sum;
-      this.heap[index].min = Math.min(this.heap[2 * index].min, this.heap[2 * index + 1].min);
-    }
-  }
-  rangeSumQuery(index = 1, from, to) {
+
+  rangeSumQuery(from, to, index = 1) {
     const node = this.heap[index];
 
     // If you did a range update that contained this node, you can infer
     // the Sum without going down the tree
-    if (node.pendingVal != null && contains(node.from, node.to, from, to)) {
+    if (hasNodeBeenUpdated(node, from, to)) {
       return (to - from + 1) * node.pendingVal;
     }
 
@@ -40,20 +83,21 @@ module.exports = class SegmentTree {
       return this.heap[index].sum;
     }
 
-    if (SegmentTree.intersects(from, to, node.from, node.to)) {
-      this.propagate(index);
-      const leftSum = this.rangeSumQuery(2 * index, from, to);
-      const rightSum = this.rangeSumQuery(2 * index + 1, from, to);
+    if (intersects(from, to, node.from, node.to)) {
+      propagate.call(this, index);
+      const leftSum = this.rangeSumQuery(from, to, 2 * index);
+      const rightSum = this.rangeSumQuery(from, to, 2 * index + 1);
       return leftSum + rightSum;
     }
 
     return 0;
   }
-  rangeMinQuery(index = 1, from, to) {
+
+  rangeMinQuery(from, to, index = 1) {
     const node = this.heap[index];
 
-    //If you did a range update that contained this node, you can infer the Min value without going down the tree
-    if (node.pendingVal != null && contains(node.from, node.to, from, to)) {
+    // If you did a range update that contained this node, you can infer the Min value without going down the tree
+    if (hasNodeBeenUpdated(node, from, to)) {
       return node.pendingVal;
     }
 
@@ -61,54 +105,54 @@ module.exports = class SegmentTree {
       return this.heap[index].min;
     }
 
-    if (SegmentTree.intersects(from, to, node.from, node.to)) {
-      this.propagate(index);
-      const leftMin = this.rangeMinQuery(2 * index, from, to);
-      const rightMin = this.rangeMinQuery(2 * index + 1, from, to);
+    if (intersects(from, to, node.from, node.to)) {
+      propagate.call(this, index);
+      const leftMin = this.rangeMinQuery(from, to, 2 * index);
+      const rightMin = this.rangeMinQuery(from, to, 2 * index + 1);
 
       return Math.min(leftMin, rightMin);
     }
 
-    return null;
+    return Number.MAX_VALUE;
   }
-  update(index = 1, from, to, value) {
-    //The Node of the heap tree represents a range of the array with bounds: [n.from, n.to]
+
+  rangeMaxQuery(from, to, index = 1) {
     const node = this.heap[index];
 
     if (contains(from, to, node.from, node.to)) {
-      this.change(node, value);
+      return this.heap[index].max;
+    }
+
+    if (intersects(from, to, node.from, node.to)) {
+      propagate.call(this, index);
+      const leftMax = this.rangeMaxQuery(from, to, 2 * index);
+      const rightMax = this.rangeMaxQuery(from, to, 2 * index + 1);
+
+      return Math.max(leftMax, rightMax);
+    }
+
+    return 0;
+  }
+
+  update(from, to, value, index = 1) {
+    // The Node of the heap tree represents a range of the array with bounds: [n.from, n.to]
+    const node = this.heap[index];
+
+    if (contains(from, to, node.from, node.to)) {
+      change.call(this, node, value);
     }
 
     if (node.size() === 1) return;
 
-    if (SegmentTree.intersects(from, to, node.from, node.to)) {
-      this.propagate(index);
-      this.update(2 * index, from, to, value);
-      this.update(2 * index + 1, from, to, value);
+    if (intersects(from, to, node.from, node.to)) {
+      propagate.call(this, index);
+      this.update(from, to, value, 2 * index);
+      this.update(from, to, value, 2 * index + 1);
 
       node.sum = this.heap[2 * index].sum + this.heap[2 * index + 1].sum;
       node.min = Math.min(this.heap[2 * index].min, this.heap[2 * index + 1].min);
+      node.max = Math.max(this.heap[2 * index].max, this.heap[2 * index + 1].max);
     }
-  }
-  propagate(index) {
-    const node = this.heap[index];
-    if (node.pendingVal != null) {
-      this.change(this.heap[2 * index], node.pendingVal);
-      this.change(this.heap[2 * index + 1], node.pendingVal);
-      node.pendingVal = null;
-    }
-  }
-  change(node, value) {
-    node.pendingVal = value;
-    node.sum = node.size() * value;
-    node.min = value;
-    this.array[node.from] = value;
-  }
-  static contains(from1, to1, from2, to2) {
-    return from2 >= from1 && to2 <= to1;
-  }
-  static intersects(from1, to1, from2, to2) {
-    return (from1 <= from2 && to1 >= from2) || (from1 >= from2 && from1 <= to2);
   }
 };
 
@@ -117,6 +161,7 @@ class SegmentTreeNode {
     this.pendingVal = null;
     this.sum = 0;
     this.min = 0;
+    this.max = 0;
     this.from = 0;
     this.to = 0;
   }
--- tests/segment-tree.test.js
+++ tests/segment-tree.test.js
@@ -1,12 +1,95 @@
-const SegmentTree = require('../segment-tree');
+const SegmentTree = require("../segment-tree");
+const faker = require("faker");
 
 describe("Segment Tree", function() {
+  describe("Numeric Array", function() {
+    it("should create a Segment Tree from a numeric Array", function() {
+      const expected = 3;
+      const tree = new SegmentTree([1, 2, 3]);
+      const actual = tree.size();
+      expect(actual).toEqual(expected);
+    });
 
-  it("should create a Segment Tree from a numeric Array", function () {
-    const expected = 3;
-    const tree = new SegmentTree([1,2,3]);
-    const actual = tree.size();
-    expect(actual).toEqual(expected);
+    it("should query a range in the tree for a minimum value", function() {
+      const expected = 1;
+      const tree = new SegmentTree([1, 2, 3]);
+      const actual = tree.rangeMinQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should query a range in the tree for a maximum value", function() {
+      const expected = 3;
+      const tree = new SegmentTree([1, 2, 3]);
+      const actual = tree.rangeMaxQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should query a range in the tree for a sum", function() {
+      const expected = 6;
+      const tree = new SegmentTree([1, 2, 3]);
+      const actual = tree.rangeSumQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should update a value in the tree", function() {
+      const expected = 7;
+      const tree = new SegmentTree([1, 2, 3]);
+      tree.update(1, 1, 3);
+      const actual = tree.rangeSumQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
   });
 
+  describe("Object Array", function() {
+
+    let treeBase;
+    const propertyKey = 'price';
+
+    beforeEach(function() {
+      treeBase = [];
+      for (let x = 1; x < 4; x++) {
+        treeBase.push({
+          item: faker.commerce.productName(),
+          price: x
+        });
+      }
+    });
+
+    it("should create a Segment Tree from an object Array", function() {
+      const expected = 3;
+      const tree = new SegmentTree(treeBase, true, propertyKey);
+      const actual = tree.size();
+      expect(actual).toEqual(expected);
+    });
+
+    it("should query a range in the tree for a minimum value", function() {
+      const expected = 1;
+      const tree = new SegmentTree(treeBase, true, propertyKey);
+      const actual = tree.rangeMinQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should query a range in the tree for a maximum value", function() {
+      const expected = 3;
+      const tree = new SegmentTree(treeBase, true, propertyKey);
+      const actual = tree.rangeMaxQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should query a range in the tree for a sum", function() {
+      const expected = 6;
+      const tree = new SegmentTree(treeBase, true, propertyKey);
+      const actual = tree.rangeSumQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+    it("should update a value in the tree", function() {
+      const expected = 7;
+      const tree = new SegmentTree(treeBase, true, propertyKey);
+      tree.update(1, 1, 3);
+      const actual = tree.rangeSumQuery(0, 3);
+      expect(actual).toEqual(expected);
+    });
+
+  });
 });
